587ad38b5db57ddad0eb70b845d59b9aeaa04dee
Isaac Park <isaac.a.park@gmail.com>
2015-09-28 21:14:53 -0400

    Replace Socket.io With GCM in EventsManager

--- app/src/main/java/io/khe/kenthackenough/backend/EventsManager.java
+++ app/src/main/java/io/khe/kenthackenough/backend/EventsManager.java
@@ -1,6 +1,7 @@
 package io.khe.kenthackenough.backend;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 
@@ -22,6 +23,9 @@ import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import io.khe.kenthackenough.Config;
+import io.khe.kenthackenough.GCM.GcmListener;
+import io.khe.kenthackenough.GCM.GcmRegisterer;
 import io.khe.kenthackenough.KHEApp;
 
 /**
@@ -37,6 +41,7 @@ public class EventsManager {
     private Handler uiThreadHandler;
     private Socket socket;
     private String url;
+    private Context applicationContext;
 
     /**
      * Standard constructor for a EventsManager (does not start it pulling)
@@ -61,115 +66,170 @@ public class EventsManager {
         this.url = url;
 
         uiThreadHandler = new Handler(context.getMainLooper());
+        applicationContext = context;
     }
 
     /**
      * Starts a repeated request to the server to fetch all messages
      */
     public void start() {
+        setUpGcm();
+
         timer.scheduleAtFixedRate(new TimerTask() {
-            synchronized public void run() {
-                KHEApp.queue.add(listEvents);
-                if (socket == null || !socket.connected()) {
-                    try {
-                        socket = IO.socket(url);
+            public void run() {
+                update();
+
+                // register with socket.io if GCM breaks for some reason
+                if (!GcmRegisterer.working) {
+                    if (socket == null || !socket.connected()) {
+                        Log.w(Config.DEBUG_TAG, "GCM failed so falling back on socket.io");
                         setSocketIO();
                         socket.connect();
-                    } catch (URISyntaxException e1) {
-                        Log.e("KHE2015", "API url " + url + " failed");
+                    }
+                } else {
+                    if(socket != null && socket.connected()) {
+                        Log.w(Config.DEBUG_TAG, "GCM came back so returning to it");
+                        socket.disconnect();
                     }
                 }
             }
+
         }, 0, checkDelay);
     }
 
-    public void setSocketIO(){
-        socket.on("create", new Emitter.Listener() {
+    private void update() {
+        KHEApp.queue.add(listEvents);
+    }
+
+    private void setUpGcm() {
+        GcmListener.addListener(applicationContext, "events", "create", new GcmListener.GcmMessageListener() {
             @Override
-            public void call(Object... args) {
-                Log.i("KHE2015","create events");
-
-                JSONObject json = (JSONObject) args[0];
+            public void onReceive(Bundle message) {
                 try {
-                    final Event event = Event.getFromJSON(json);
-                    uiThreadHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            events.add(0, event);
-                            for (EventsUpdateListener listener : updateListeners) {
-                                listener.eventsFetched(events);
-                            }
-                        }
-                    });
+                    Event newEvent = Event.getFromJSON(new JSONObject(message.getString("document")));
+                    createEvent(newEvent);
                 } catch (JSONException e) {
-                    Log.e("KHE2015", "failed to parse create message", e);
-                }
-
-            }
-        });
-
-        socket.on("delete", new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                Log.i("KHE2015","delete events");
-
-                JSONObject json = (JSONObject) args[0];
-                try {
-                    String uuidString = json.getString("_id");
-                    final Long[] id = new Long[2];
-                    id[0] = Long.decode('#' + uuidString.substring(0, 12));
-                    id[1] = Long.decode('#' + uuidString.substring(12));
-
-                    uiThreadHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            events.remove(new Event(id));
-                            for (EventsUpdateListener listener : updateListeners) {
-                                listener.eventsFetched(events);
-                            }
-                        }
-                    });
-
-                } catch (JSONException e) {
-                    Log.e("KHE2015", "failed to parse delete message", e);
+                    Log.e(Config.DEBUG_TAG, "failed to parse create message", e);
                 }
             }
         });
 
-        socket.on("update", new Emitter.Listener() {
-
+        GcmListener.addListener(applicationContext, "events", "delete", new GcmListener.GcmMessageListener() {
             @Override
-            public void call(Object... args) {
-                Log.i("KHE2015","updating events");
+            public void onReceive(Bundle message) {
                 try {
-                    final Event updatedEvent = Event.getFromJSON((JSONObject) args[0]);
-                    uiThreadHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            events.remove(updatedEvent);
-                            events.add(updatedEvent);
-                            Collections.sort(events);
-
-                            for (EventsUpdateListener listener : updateListeners) {
-                                listener.eventsFetched(events);
-                            }
-                        }
-                    });
+                    deleteEvent(new JSONObject(message.getString("document")).getString("_id"));
                 } catch (JSONException e) {
-                    Log.e("KHE2015", "failed to parse update message", e);
+                    Log.e(Config.DEBUG_TAG, "failed to parse delete message", e);
+                }
+            }
+        });
+
+        GcmListener.addListener(applicationContext, "events", "update", new GcmListener.GcmMessageListener() {
+            @Override
+            public void onReceive(Bundle message) {
+                try {
+                    Event updatedEvent = Event.getFromJSON(new JSONObject(message.getString("document")));
+                    updateEvent(updatedEvent);
+                } catch (JSONException e) {
+                    Log.e(Config.DEBUG_TAG, "failed to parse update message", e);
                 }
             }
         });
     }
 
-    /**
-     * Stops the requests started by start()
-     */
-    public void halt() {
-        timer.purge();
-        KHEApp.queue.cancelAll("listMessages");
+    private void setSocketIO(){
+        try {
+            socket = IO.socket(url);
+
+            socket.on("create", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    JSONObject json = (JSONObject) args[0];
+                    try {
+                        final Event event = Event.getFromJSON(json);
+                        uiThreadHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                createEvent(event);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        Log.e(Config.DEBUG_TAG, "failed to parse create event", e);
+                    }
+
+                }
+            });
+
+            socket.on("delete", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    JSONObject json = (JSONObject) args[0];
+                    try {
+                        final String uuidString = json.getString("_id");
+                        uiThreadHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                deleteEvent(uuidString);
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        Log.e(Config.DEBUG_TAG, "failed to parse delete event", e);
+                    }
+                }
+            });
+
+            socket.on("update", new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    Log.i(Config.DEBUG_TAG,"updating events");
+                    try {
+                        final Event updatedEvent = Event.getFromJSON((JSONObject) args[0]);
+                        uiThreadHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateEvent(updatedEvent);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        Log.e(Config.DEBUG_TAG, "failed to parse update event", e);
+                    }
+                }
+            });
+        } catch (URISyntaxException e) {
+            Log.e(Config.DEBUG_TAG, "Failed to connect to " + url + " with socket.io");
+        }
     }
 
+    private void createEvent(Event newEvent) {
+        events.add(0, newEvent);
+        for (EventsUpdateListener listener : updateListeners) {
+            listener.eventsFetched(events);
+        }
+    }
+
+    private void deleteEvent(String uuidString) {
+        final Long[] id = new Long[2];
+        id[0] = Long.decode('#' + uuidString.substring(0, 12));
+        id[1] = Long.decode('#' + uuidString.substring(12));
+
+        events.remove(new Event(id));
+        for (EventsUpdateListener listener : updateListeners) {
+            listener.eventsFetched(events);
+        }
+    }
+
+    private void updateEvent(Event updatedEvent) {
+        events.remove(updatedEvent);
+        events.add(updatedEvent);
+        Collections.sort(events);
+
+        for (EventsUpdateListener listener : updateListeners) {
+            listener.eventsFetched(events);
+        }
+    }
     /**
      * Adds a listener
      *
